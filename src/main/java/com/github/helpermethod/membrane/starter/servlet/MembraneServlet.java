3089dece4fbb05bc3dc7df3f24840c67b149637e
oliver <weiler@predic8.de>
2017-04-24 21:24:45 +0200

    Add MembraneController

--- src/main/java/com/github/helpermethod/membrane/starter/servlet/MembraneServlet.java
+++ src/main/java/com/github/helpermethod/membrane/starter/servlet/MembraneServlet.java
@@ -1,7 +1,14 @@
 package com.github.helpermethod.membrane.starter.servlet;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class MembraneServlet extends HttpServlet {
-
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        super.service(request, response);
+    }
 }
