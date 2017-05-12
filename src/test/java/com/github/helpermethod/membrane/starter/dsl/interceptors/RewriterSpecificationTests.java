aae3104bd123b8b8ab41b78d6a50b6bccb5f7792
oliver <weiler@predic8.de>
2017-05-12 09:19:56 +0200

    Add RewriterSpecification tests

---
+++ src/test/java/com/github/helpermethod/membrane/starter/dsl/interceptors/RewriterSpecificationTests.java
@@ -0,0 +1,27 @@
+package com.github.helpermethod.membrane.starter.dsl.interceptors;
+
+import com.predic8.membrane.core.interceptor.rewrite.RewriteInterceptor.Mapping;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static com.predic8.membrane.core.interceptor.rewrite.RewriteInterceptor.Type.REDIRECT_TEMPORARY;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.tuple;
+
+@DisplayName("A RewriterSpecification")
+class RewriterSpecificationTests {
+
+    @DisplayName("should add a mapping to the list of rewrite interceptor mappings")
+    @Test
+    void testMap() {
+        ArrayList<Mapping> mappings = new ArrayList<>();
+        RewriterSpecification rewriterSpecification = new RewriterSpecification(mappings);
+        rewriterSpecification.map("http://www.predic8.de", "http://www.p8.de");
+
+        assertThat(mappings).hasSize(1)
+                            .extracting("from", "to", "do")
+                            .contains(tuple("http://www.predic8.de", "http://www.p8.de", REDIRECT_TEMPORARY));
+    }
+}
