82a94ca4428e41ed4f9922a73d47e488e10d80bb
oliver <weiler@predic8.de>
2017-05-28 17:20:33 +0200

    Add method overload

--- src/main/java/com/predic8/membrane/starter/dsl/MatcherSpecification.java
+++ src/main/java/com/predic8/membrane/starter/dsl/MatcherSpecification.java
@@ -2,6 +2,7 @@ package com.predic8.membrane.starter.dsl;
 
 import com.predic8.membrane.core.config.Path;
 import com.predic8.membrane.core.rules.ServiceProxy;
+import org.springframework.http.HttpMethod;
 
 public class MatcherSpecification {
     private final ServiceProxy serviceProxy;
@@ -14,6 +15,10 @@ public class MatcherSpecification {
         serviceProxy.setMethod(method);
 
         return this;
+    }
+
+    public MatcherSpecification method(HttpMethod method) {
+        return method(method.toString());
     }
 
     public MatcherSpecification pathPrefix(String path) {
